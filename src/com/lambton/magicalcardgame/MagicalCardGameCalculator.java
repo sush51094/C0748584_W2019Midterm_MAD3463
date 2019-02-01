fefebcbc54c8e182746683f0ea006520511faa19
Sushmitha <sushmitha51094@gmail.com>
2019-01-31 21:12:30 -0500

    added string

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -1,35 +1,43 @@
-package com.lambton.magicalcardgame;
+package com.lambton.magicalcardgame ;
 
-public class MagicalCardGameCalculator
-
-{
-int s1;
-String s2;
+public class MagicalCardGameCalculator implements I {
+    int s1;
+    String s2;
 
 
-    public void FirstShuffleResult(String[][] cardlist,int s )
+    public String[][] getFirstShuffleResult(Magiccardgamemodel magicalCardGameModel) {
+        int s1 = s;
+        if (s1 == s)
 
-
-    {
-        int s1=s;
-        if(s1==s)
-
-            for(int i=0;i<2;i++)
-            {
-                for(int j=0;j<2;j++)
-                {
-                    cardlist[i][j]=cardlist[j][i];
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    cardlist[i][j] = cardlist[j][i];
                 }
             }
-        for(int i=0;i<2;i++)
-        {
-            for ( int j=0;j<2;j++)
-            {
-                cardlist[i][j]=cardlist[j][i];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                cardlist[i][j] = cardlist[j][i];
             }
         }
 
     }
-else if
-    return(cardlist[][])
+    else
+    {
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<2;j++)
+            {
+                cardlist[i][j] = cardlist[j][i];
+            }
+        }
+
+        for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<2;j++)
+        }
+
+    }
 }
+
+
+
