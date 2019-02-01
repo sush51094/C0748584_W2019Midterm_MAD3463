f63f4b3111c1b1c1a0598bf003cefcefd26a6fb7
Sushmitha <sushmitha51094@gmail.com>
2019-01-31 19:27:05 -0500

    no message

--- src/com/lambton/magicalcardgame/Magiccardgamemodel.java
+++ src/com/lambton/magicalcardgame/Magiccardgamemodel.java
@@ -1,4 +1,28 @@
 package com.lambton.magicalcardgame;
 
+
 public class Magiccardgamemodel {
+
+    String firstShuffleColPos;
+
+    String secShuffleColPos;
+
+    String[][] cardList = {{"A", "4", "3"},
+            {"K", "7", "2"},
+            {"5", "9", "8"}};
+    String[][] firstShuffle = null;
+
+    String[][] secShuffle = null;
+
+    String guessedCard;
+
+    public void display() {
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                System.out.print(cardList[i][j] +" ");
+
+            }
+System.out.println();
+        }
+    }
 }
