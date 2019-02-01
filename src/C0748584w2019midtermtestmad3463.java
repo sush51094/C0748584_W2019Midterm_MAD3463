374b45f2e7ed5d9076abc5e79616c8ec00718ec3
Sushmitha <sushmitha51094@gmail.com>
2019-01-31 20:24:16 -0500

    added scanner

--- src/C0748584w2019midtermtestmad3463.java
+++ src/C0748584w2019midtermtestmad3463.java
@@ -1,14 +1,21 @@
 import com.lambton.magicalcardgame.Magiccardgamemodel;
 
+import java.util.Scanner;
+
 public class C0748584w2019midtermtestmad3463 {
 
     public static void main(String [] args)
     {
         Magiccardgamemodel obj= new Magiccardgamemodel();
 
-        obj.display();
         System.out.println("please enter the card");
 
+        obj.display();
+
         System.out.println("please enter the column from which the card has been choose" );
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
     }
 }
