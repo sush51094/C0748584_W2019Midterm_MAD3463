7bd7a31a96ec5cae08198721ec972487c43afab6
Sushmitha <sushmitha51094@gmail.com>
2019-01-31 21:22:16 -0500

    second method

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -9,7 +9,7 @@ public class MagicalCardGameCalculator implements I {
         int s1 = s;
         if (s1 == s)
 
-            for (int i = 0; i < 2; i++) {
+           for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < 2; j++) {
                     cardlist[i][j] = cardlist[j][i];
                 }
@@ -22,21 +22,70 @@ public class MagicalCardGameCalculator implements I {
 
     }
     else
+
     {
-        for(int i=0;i<2;i++)
-        {
-            for(int j=0;j<2;j++)
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                card list[i][j] = cardlist[j][i];
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                cardlist[i][j] = cardlist[j][i];
+
+                System.out.print(cardlist[i][j]);
+
+
+            }
+
+            public String[][] getSecShuffleResult(Magiccardgamemodel magicalCardGameModel)
+
             {
+                for (int i = 0; i < 2; i++) {
+                    for (int j = 0; j < 2; j++) {
+                        cardlist[i][j] = cardlist[j][i];
+                    }
+                }
+            }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
                 cardlist[i][j] = cardlist[j][i];
             }
         }
 
-        for(int i=0;i<2;i++)
-        {
-            for(int j=0;j<2;j++)
+    }
+    else
+
+    {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                card list[i][j] = cardlist[j][i];
+            }
         }
 
-    }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                cardlist[i][j] = cardlist[j][i];
+
+                System.out.print(cardlist[i][j]);
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ]
 }
 
 
