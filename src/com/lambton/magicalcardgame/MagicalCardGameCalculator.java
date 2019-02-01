2b2028aa71aa9656dab72bc398833431c8d69c22
Sushmitha <sushmitha51094@gmail.com>
2019-01-31 20:32:03 -0500

    added string method

--- src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
+++ src/com/lambton/magicalcardgame/MagicalCardGameCalculator.java
@@ -3,6 +3,13 @@ package com.lambton.magicalcardgame;
 public class MagicalCardGameCalculator
 
 {
+String s1;
+String s2;
 
+    public String[][]FirstShuffleResult(String s)
+    {
+        if(s1==s2)
+
+    }
 
 }
