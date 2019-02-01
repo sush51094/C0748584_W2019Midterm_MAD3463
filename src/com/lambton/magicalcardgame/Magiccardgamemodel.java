d009f323e9c3ec4c9ce4dd70a4f9f72ed5757025
Sushmitha <sushmitha51094@gmail.com>
2019-01-31 19:28:05 -0500

    no message

--- src/com/lambton/magicalcardgame/Magiccardgamemodel.java
+++ src/com/lambton/magicalcardgame/Magiccardgamemodel.java
@@ -16,6 +16,54 @@ public class Magiccardgamemodel {
 
     String guessedCard;
 
+    public String getFirstShuffleColPos() {
+        return firstShuffleColPos;
+    }
+
+    public void setFirstShuffleColPos(String firstShuffleColPos) {
+        this.firstShuffleColPos = firstShuffleColPos;
+    }
+
+    public String getSecShuffleColPos() {
+        return secShuffleColPos;
+    }
+
+    public void setSecShuffleColPos(String secShuffleColPos) {
+        this.secShuffleColPos = secShuffleColPos;
+    }
+
+    public String[][] getCardList() {
+        return cardList;
+    }
+
+    public void setCardList(String[][] cardList) {
+        this.cardList = cardList;
+    }
+
+    public String[][] getFirstShuffle() {
+        return firstShuffle;
+    }
+
+    public void setFirstShuffle(String[][] firstShuffle) {
+        this.firstShuffle = firstShuffle;
+    }
+
+    public String[][] getSecShuffle() {
+        return secShuffle;
+    }
+
+    public void setSecShuffle(String[][] secShuffle) {
+        this.secShuffle = secShuffle;
+    }
+
+    public String getGuessedCard() {
+        return guessedCard;
+    }
+
+    public void setGuessedCard(String guessedCard) {
+        this.guessedCard = guessedCard;
+    }
+
     public void display() {
         for (int i = 0; i <= 2; i++) {
             for (int j = 0; j <= 2; j++) {
